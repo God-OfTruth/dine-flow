bd5f7361fda6ea603f12c3d5c4d0a1e0fdbcb337
God-OfTruth <6386391ritesh@gmail.com>
2024-12-02 14:49:57 +0530

    added materail and tailwind

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { MobileComponent } from './layout/mobile/mobile.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'home'
+    },
+    {
+        path: 'home',
+        component: MobileComponent
+    }
+];
---
+++ src/app/layout/mobile/mobile.component.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mobile',
+  imports: [CommonModule],
+  templateUrl: './mobile.component.html',
+})
+export class MobileComponent {
+
+}
